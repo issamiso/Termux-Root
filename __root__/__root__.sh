bac7cb4de6d06925df1af1beef19a133b9af5c65
issamiso <101994309+issamiso@users.noreply.github.com>
2022-12-24 19:09:38 +0100

    Add files via upload

---
+++ __root__/__root__.sh
@@ -0,0 +1,6 @@
+# root kali
+cd $HOME&&pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Kali/kali.sh && bash kali.sh
+
+
+
+
